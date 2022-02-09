6d78665008444f07168c8c6fd860f7d61ba35793
Aaron Sando <Aaron@Charactercabinets.com.au>
2022-02-09 09:01:21 +0800

    updating mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/StoneAndCabinetsOffcutStore', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://Sandoman:Charactergroup01*@cluster0.shff0.mongodb.net/StoneAndCabientsOffcuts?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
